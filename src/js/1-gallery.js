8827d5a9fdbe9fcd13ee68865aae0b8f964fcb37
Andrii Kirieiev <kirieiev.andrii@gmail.com>
2024-02-11 04:39:47 +0200

    fourth-commit

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,81 @@
-console.log('Gallry');
+'use strict';
+
+import { images } from './gallery-images.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const imageContainer = document.querySelector('.gallery');
+imageContainer.insertAdjacentHTML('beforeend', createMarkup(images));
+// imageContainer.innerHTML = createMarkup(images);
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery-item">
+        <a class="gallery-link" href="${original}">
+        <img
+        class="gallery-image"
+        src="${preview}"
+        alt="${description}"/>
+        </a>
+    </li>`
+    )
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+// second variant
+
+// const markup = images.reduce(
+//   (acc, { original, preview, description }) =>
+//     (acc += `<li>
+//   <a class="gallery__item" href="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       alt="${description}"
+//     />
+//   </a>
+// </li>`),
+//   ''
+// );
+
+// second variant/
+
+// old code
+
+// imageContainer.addEventListener('click', handleImageClick);
+
+// function handleImageClick(event) {
+//   if (event.target === event.currentTarget) {
+//     return;
+//   }
+
+//   event.preventDefault();
+
+//   const clickedImage = event.target.closest('.gallery-item');
+//   const modalImage = clickedImage.querySelector('.gallery-link').href;
+
+//   document.addEventListener('keydown', event => {
+//     if (event.key === 'Escape' && instance) {
+//       instance.close();
+//     }
+//   });
+
+//   const instance = basicLightbox.create(`
+//         <div class="modal">
+//             <img class="modal-image" src="${modalImage}" alt="${
+//     clickedImage.querySelector('.gallery-image').alt
+//   }">
+//         </div>
+//   `);
+
+//   instance.show();
+// }
+
+// old code/
--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,42 @@
-console.log('Form');
+'use strict';
+
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', onTextField);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  if (input.value !== '' && textarea.value !== '') {
+    localStorage.removeItem(STORAGE_KEY);
+    console.log({ email: input.value, message: textarea.value });
+    event.currentTarget.reset();
+  }
+}
+
+function onTextField(event) {
+  const savedFieldItem = localStorage.getItem(STORAGE_KEY);
+  const parsedFieldItem = JSON.parse(savedFieldItem);
+  const formFieldItems = {
+    input: parsedFieldItem?.input,
+    textarea: parsedFieldItem?.textarea,
+  };
+  if (event.target.tagName === 'INPUT') {
+    const inputValue = event.target.value.trim();
+    formFieldItems.input = inputValue;
+  } else if (event.target.tagName === 'TEXTAREA') {
+    const textareaValue = event.target.value.trim();
+    formFieldItems.textarea = textareaValue;
+  }
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formFieldItems));
+}
+
+const storedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+const savedInput = storedData?.input ? storedData.input : '';
+const savedTextarea = storedData?.textarea ? storedData.textarea : '';
+
+input.value = savedInput;
+textarea.value = savedTextarea;
